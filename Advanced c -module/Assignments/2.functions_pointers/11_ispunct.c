8a4877f2d5a1d53123554b976758b08bfcaa7b78
kiruthikasarvanakumar <160112638+kiruthikasarvanakumar@users.noreply.github.com>
2024-03-25 10:32:12 +0530

    Create 11_ispunct.c

---
+++ Advanced c -module/Assignments/2.functions_pointers/11_ispunct.c
@@ -0,0 +1,48 @@
+/*
+Name : S.Kiruthika
+Date : 25-3-2024
+Description : WAP to implement your own ispunct() function
+Sample input : Enter the character: a
+Sample output : Entered character is not an punctuation character
+*/
+
+#include <stdio.h>
+
+//function prototype
+int my_isalnum(char);
+
+int main()
+{
+    char ch;
+    int ret;
+    
+    //read the input from the user
+    scanf("%c", &ch);
+    
+    //call the function
+    ret = my_isalnum(ch);
+    
+    
+     if ( ret == 1 )
+    {
+        //if the return value is 1 , print punctuation character
+	printf("Entered character is punctuation character\n");
+    }
+    else
+    {
+        //if the return value is not 1 , print not an punctuation character
+	printf("Entered character is not punctuation character\n");
+    }
+}
+
+int my_isalnum(char ch)                                                         
+{
+    // checking punctuation character
+    if ((ch >= '!' && ch <= '/') || (ch >= ':' && ch <= '@') || (ch >= '[' && ch <= '`') || (ch >= '{' && ch <= '~')) {
+	return 1;
+    }
+    else
+    {
+	return 0;
+    }
+}
