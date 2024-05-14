f8e5d1a46759c9bb05fe18136225fefd1c31d2d2
kiruthikasarvanakumar <160112638+kiruthikasarvanakumar@users.noreply.github.com>
2024-05-14 20:27:11 +0530

    Update test_encode.c

--- Advanced c -module/C-Project-1/4-SkeletonCode/test_encode.c
+++ Advanced c -module/C-Project-1/4-SkeletonCode/test_encode.c
@@ -1,3 +1,81 @@
+/*
+Name: S.KIRUTHIKA
+
+Date of Submission: 12-05-2024
+
+Project Name:LSB Image Steganography
+
+Description:  Steganography is the art and science of concealing information within other 
+              non-secret data, such as images, audio files, or text, in a way that the existence of 
+              the hidden information is undetectable to the casual observer. It operates on the principle 
+              that the secret message is embedded within a carrier medium in such a way that it is invisible to anyone 
+              not aware of its presence.
+Input CLAs:
+		1. -e (for Encoding)
+			2. Innocent image (BMP image file) 
+			3. Secret file (TXT file)
+			4. Stego image's name [Optional]
+		
+		1. -d (for Decoding)
+			2. Stego image file
+			3. Decode file's name [Optional]
+Sample execution: -
+
+Test Case 1:
+
+./a.out -e beautiful.bmp secret.txt stego.bmp
+###ENCODING PROCEDURE STARTED###
+Read and validate encode argument successful
+Width = 1024
+Height = 768
+INFO: Opening required files
+INFO: Done.
+Check capacity is successful
+INFO: Done.
+INFO: Copying Image Header
+INFO: Done.
+INFO: Encoded Magic String
+INFO: Done.
+INFO: Encoded secret.txt File Size Extension
+INFO: Done.
+INFO: Encoded secret.txt File Extension
+INFO: Done.
+INFO: Encoded secret.txt File Data
+INFO: Done.
+INFO: Encoded secret.txt File Size
+INFO: Done.
+INFO: Copying Left Over Data
+INFO: Done.
+###ENCODING DONE SUCCESSFULLY###
+
+Test Case 2:
+
+./a.out -d stego.bmp output.txt
+
+###DECODING PROCEDURE STARTED###
+Read and validate decode argument successful
+INFO: Opening Required Files
+INFO: Decoding Magic String
+INFO: Done.
+INFO: Decoded Output File extension size
+INFO: Done.
+INFO: Output File not mentioned. Creating decoded.txt as default
+INFO: Opened decoded.txt
+INFO: Done.Opened All Required Files
+INFO: Decoding File Size
+INFO: Done.
+INFO: Decoding File Data
+INFO: Done
+###DECODING DONE SUCCESSFULLY###
+
+Test Case 3:
+
+./a.out
+
+Please pass required CLAs
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include "encode.h"
