ffc4a18a68cd1910cb746c317efb3937fec34308
kiruthikasarvanakumar <160112638+kiruthikasarvanakumar@users.noreply.github.com>
2024-06-22 10:54:25 +0530

    Update main.c

--- MPLABXProjects/A1_LED_TRAIN.X/main.c
+++ MPLABXProjects/A1_LED_TRAIN.X/main.c
@@ -1,58 +1,65 @@
 /*
  * Name: S.Kiruthika
- * Date of Submission : 20-06-2024
- * Description: Implement LED train pattern on leds
+ * Date of Submission: 21-06-2024
+ * Description: Embedded C program to display a pattern on the LEDs.
+                This program will toggle the LEDs at PORTD
  */
 
 #include <xc.h>
 #include "main.h"
 
+// Configuration Bit settings
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-/* Function: init_config()
- * Description: This function is used to setup initial peripheral
- * configuration, like setting port directions, initializing port values
- */
+
+// Function Prototypes
+static void init_config(void);
+
+// Function: init_config()
+// Description: This function is used to setup initial peripheral
+// configuration, like setting port directions, initializing port values
+
 static void init_config(void) {
-    LED_ARRAY1_DDR = 0x00; // Set direction to o/p
-    LED_ARRAY1 = 0x01;     // Set initial port value to the first LED on
+    LED_ARRAY1_DDR = 0x00; // Set PORTB direction to output
+    LED_ARRAY1 = 0x00;     // Initialize all LEDs to be off
 }
 
-void main(void) 
-{
+void main(void) {
     unsigned int wait = 0;
-    unsigned char i = 1;   // 1 for left to right, 0 for right to left
-    
+    unsigned char i = 0;
+
     init_config();
 
-    while (1) 
-    {
-        if(wait++ == 50000)
-        {
+    while (1) {
+        if (wait++ == 50000) {
             wait = 0;
 
-            // Update LED state
-            if (i) 
-            {
-                // Left to right
-                LED_ARRAY1 <<= 1;
-                if (LED_ARRAY1 == 0) 
-                {
-                    LED_ARRAY1 = 0x80; // Reset to the last LED
-                    i = 0; // Change direction to right to left
-                }
-            } 
-            else 
-            {
-                // Right to left
-                LED_ARRAY1 >>= 1;
-                if (LED_ARRAY1 == 0) 
-                {
-                    LED_ARRAY1 = 0x01; // Reset to the first LED
-                    i = 1; // Change direction to left to right
-                }
+            // Condition to turn on LEDs from top to bottom
+            if (i <= 7) {
+                LED_ARRAY1 |= (1 << i); 
+                i++;
+            }
+            // Condition to turn off LEDs from top to bottom
+            else if (i > 7 && i <= 15) {
+                LED_ARRAY1 &= ~(1 << (i - 8)); 
+                i++;
+            }
+            // Condition to turn on LEDs from bottom to top
+            else if (i > 15 && i <= 23) {
+                LED_ARRAY1 |= (1 << (7 - (i - 16))); 
+                i++;
+            }
+            // Condition to turn off LEDs from bottom to top
+            else if (i > 23 && i < 32) {
+                LED_ARRAY1 &= ~(1 << (7 - (i - 24))); 
+                i++;
+            }
+            // Reset the sequence
+            else {
+                i = 0;
+                LED_ARRAY1 = 0x00; // Turn off all LEDs
             }
         }
     }
+
     return;
 }
-
